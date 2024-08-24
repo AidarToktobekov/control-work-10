d0243365e53a02fcee1d1a29babb59626bc8d8c3
Toktobekov Aidar <aidar.omurbekov05@gmail.com>
2024-08-24 15:29:17 +0600

    Установил все нужные библиотеки

--- news-api/index.ts
+++ news-api/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import fileDb from "./fileDb";
 import commentsRouter from "./routers/comments";
 import newsRouter from "./routers/news";
+import cors, { CorsOptions} from "cors";
 
 const app = express();
 const port = 8080;
@@ -10,6 +11,18 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use('/', commentsRouter);
 app.use('/', newsRouter);
+const whiteList = ['http://localhost:5173'];
+const corsOptions: CorsOptions = {
+    origin: (origin, callback)=>{
+        if(!origin || whiteList.indexOf(origin) !== -1){
+            callback(null, true);
+        }else{
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+}
+
+app.use(cors(corsOptions));
 
 const run = async()=>{
     await fileDb.init();
